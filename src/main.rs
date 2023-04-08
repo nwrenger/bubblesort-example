4e28e497521579e2a74af92d1d5e8657f52d1ac4
NWrenger <nils@wrenger.net>
2023-04-08 23:59:51 +0200

    :sparkles: Fixed performance for edge case

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,30 @@ fn main() {
 fn cocktailshakersort(vec: &mut [i32]) {
     let mut end = vec.len() - 1;
     let mut start = 0;
+    let mut swapped = true;
 
-    while start < end {
+    while start < end && swapped {
+        swapped = false;
         for i in start..end {
             if vec[i] > vec[i + 1] {
                 vec.swap(i, i + 1);
+                swapped = true;
             }
         }
+        if !swapped {
+            break;
+        }
         end -= 1;
-
+        swapped = false;
         for i in (start..end).rev() {
             if vec[i] > vec[i + 1] {
                 vec.swap(i, i + 1);
+                swapped = true;
             }
+        }
+
+        if !swapped {
+            break;
         }
         start += 1;
     }
